2cac0eaf8950a8b377e6bd539db3650aa230367e
Vijayasai Sanagaram <vijayasai10@gmail.com>
2021-07-23 15:37:51 +0530

    no of hits

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'to-do-planner';
+
+  constructor(){
+    
+  }
+
+  ngOnInit(){
+    this.calculateHits()
+  }
+
+  calculateHits(){
+    var n = localStorage.getItem('hitCounter');
+    if (n === null) {
+      n = '0'
+      var hitCount = Number(n).valueOf();
+      console.log(hitCount)
+    }
+    else{
+      var hitCount = Number(n).valueOf();
+      hitCount++;
+      n = hitCount.toString();
+    }
+
+    localStorage.setItem("hitCounter", n);
+
+    var hitCount = Number(n).valueOf();
+    if ((hitCount % 50) == 0){
+      // Collect IP Address and send that to authentication server
+    }
+  }
+
+
 }
