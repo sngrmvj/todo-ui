29d6e37b4aeb2da246337359f1efeeb558326458
Vijayasai Sanagaram <vijayasai10@gmail.com>
2021-08-06 10:27:23 +0530

    Implemented Partially Feedback

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -12,4 +12,10 @@ export class AboutComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  submitFeedback(value:any){
+    console.log(value.feedback);
+    console.log("Need to get the email of the user to store that feedback");
+    // We can use redis for saving the feedback
+  }
+
 }
