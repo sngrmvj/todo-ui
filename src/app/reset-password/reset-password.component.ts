98627d294863bb144cb8e3750fc61923c707471f
Vijayasai Sanagaram <vijayasai10@gmail.com>
2021-08-09 11:57:45 +0530

    reset-password

--- src/app/reset-password/reset-password.component.ts
+++ src/app/reset-password/reset-password.component.ts
@@ -8,7 +8,9 @@ import { Router } from '@angular/router';
 })
 export class ResetPasswordComponent implements OnInit {
 
-  isPasswordMatch: boolean = false;
+  isPasswordMatch: boolean = true;
+  isLengthSatisfied: boolean = true;
+  lengthMismatchString: string = "";
   hide:boolean = true;
 
   
@@ -18,10 +20,21 @@ export class ResetPasswordComponent implements OnInit {
   }
 
 
-  onSubmit(){
-    // API Call pass the refresh token so that it contains the DBID. 
-    // It will be easy to update the password for the user using the user id
-    this.router.navigate(['login'])
+  onSubmit(resetForm:any){
+    if(resetForm.password === resetForm.confirm_password ){
+      if (resetForm.password.length >= 15){
+      // API Call pass the refresh token so that it contains the DBID. 
+      // It will be easy to update the password for the user using the user id
+      this.router.navigate(['login'])
+      }
+      else{
+        this.isLengthSatisfied = false;
+        this.lengthMismatchString = `The length of the password should be 15 characters.`;
+      }
+    }
+    else{
+      this.isPasswordMatch = false;
+    }
   }
 
 }
